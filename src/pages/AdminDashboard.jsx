180bb7ced159ea3c965133ddd749a10e6777c641
NickshanJ <nickshan001@gmail.com>
2025-03-01 20:47:44 +0530

    Update

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { Link, Outlet, useNavigate } from 'react-router-dom';
-import CreateBookingForm from '../components/CreateBookingForm'; 
 import axios from 'axios';
 
 const AdminDashboard = () => {
