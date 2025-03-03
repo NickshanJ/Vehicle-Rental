9c7a8c0435a941529dbca63c579dbdd840583faa
NickshanJ <nickshan001@gmail.com>
2025-03-03 20:37:48 +0530

    Update

--- src/components/ReviewModal.jsx
+++ src/components/ReviewModal.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import { AiOutlineCheckCircle } from 'react-icons/ai';
 
 const ReviewModal = ({ isOpen, onClose, message }) => {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 z-50 flex bg-[#ffffff2c] bg-opacity-50 items-center justify-center">
-      <div className="relative bg-white p-8 rounded-lg shadow-lg w-11/12 md:w-1/3">
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-[#eeeeee5e] bg-opacity-50 backdrop-blur-sm">
+      <div className="relative bg-white p-8 rounded-lg shadow-lg w-11/12 md:w-1/3 animate-fadeIn">
+        <AiOutlineCheckCircle className="text-green-500 text-6xl mx-auto mb-4 animate-bounce" />
         <h2 className="text-2xl font-bold mb-4 text-center text-blue-500">{message}</h2>
         <button
           className="bg-blue-500 text-white font-bold px-4 py-2 rounded hover:bg-blue-600 mx-auto block"
